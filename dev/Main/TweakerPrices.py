fb24979f88b1c78687f0582f596b0d8c6c122603
dopeeh <38014435+dopeeh@users.noreply.github.com>
2018-04-19 12:44:36 +0200

    Updated
    Some code changes and format change.

--- dev/Main/TweakerPrices.py
+++ dev/Main/TweakerPrices.py
@@ -9,7 +9,11 @@ import datetime
 from pkg_resources import resource_dir
 import six
 
-successList = []
+successIndex = 0
+productLijst = []
+vendorpnLijst = []
+winkelLijst = []
+prijsLijst = []
 
 #Checking folders and making necessary changes/references
 rootFolder = sys.path[0]
@@ -51,9 +55,9 @@ if not vendor_list == None:
             driver.find_element_by_name("decision").click()
 
         driver.find_element_by_name("keyword").send_keys(product)
-        for i in range(0, 2):
+        for i in range(0, 3):
             driver.find_element_by_xpath("//input[@value='Zoeken']").click()
-        #time.sleep(1)
+        time.sleep(1)
 
         #Try because by default selenium throws exceptions when it can't find certain elements
         try:
@@ -74,47 +78,39 @@ if not vendor_list == None:
 
             #Navigating to the right page if you landed on a wrong page
             if checkPage() == False:
-                #time.sleep(1)
                 driver.find_element_by_xpath(".//tr[@class='largethumb']/td[@class='itemname']/p/a").click()
             else:
                 debugvar = "Success"
 
-            print("Success!")
 
-            productLijst = []
-            vendorpnLijst = []
-            winkelLijst = []
             winkelLijstElementen = driver.find_elements_by_xpath("//div[@id='listing']/table/tbody//p[@class='ellipsis']/a")
             for i in winkelLijstElementen:
                 winkelLijst.append(i.text)
                 productLijst.append(filteredPN)
                 vendorpnLijst.append(filteredVN)
 
-
-            prijsLijst = []
             prijsLijstElementen = driver.find_elements_by_xpath("//div[@id='listing']/table/tbody//td[@class='shop-price']/p/a")
             for i in prijsLijstElementen:
                 prijsLijst.append(i.text)
 
-            result_notzipped = [productLijst,
-                      vendorpnLijst,
-                      winkelLijst,
-                      prijsLijst]
-            result = zip(*result_notzipped)
+            print("Success!")
+            successIndex += 1
 
-            #Output the zipped results as CSV with 4 rows
-            outputfile = directory_output + "\\" + filteredPN + ".csv"
-
-            with open(outputfile, 'w') as output:
-                writer = csv.writer(output)
-                writer.writerows(result)
-
-            successList.append(product)
-        except:
+        except Exception as e:
             print("Product '" + str(product) + "' not found.")
+            print(e)
 
         #time.sleep(1)
         driver.quit()
 
-print("Successfully scraped {0} products!".format(len(successList)))
+# Output the results as CSV with 4 rows
+outputfile = directory_output + "\\" "Result - " + datetime.date.today().strftime("%B-%d-%Y") + ".csv"
+result = list(zip(productLijst, vendorpnLijst, winkelLijst, prijsLijst))
+
+with open(outputfile, 'w') as output:
+    writer = csv.writer(output)
+    writer.writerows(result)
+
+
+print("Successfully scraped {0} products!".format(successIndex))
 os.system('pause')
